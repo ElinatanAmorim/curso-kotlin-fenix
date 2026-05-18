62fab1f7cab90d1816275286a2fb760e33ea078e
Elinatan Amorim <elinatanamorim@gmail.com>
2026-05-17 21:25:25 -0300

    Implementação para os valores das variáveis de a e b sejam passados na console

--- src/main/kotlin/conteudo/funcoes/exercicioCalculadoraSimples/CalculadoraSimples.kt
+++ src/main/kotlin/conteudo/funcoes/exercicioCalculadoraSimples/CalculadoraSimples.kt
@@ -3,11 +3,16 @@ package conteudo.funcoes.exercicioCalculadoraSimples
 fun main() {
 
     var calculo = CalculosSimples()
-    val a = 10.0
-    val b = 0.0
+//    val a = 10.0
+//    val b = 0.0
+    println("Digite o valor de a: ")
+    var a = readln().toDouble()
+
+    println("Digite o valor de b: ")
+    var b = readln().toDouble()
+
     println("o valor da soma é " + calculo.adicao(a, b))
     println("o valor da subtração é " + calculo.subtracao(a, b))
     println("o valor da multiplicação é " + calculo.multiplicacao(a, b))
     println(calculo.divisao(a, b))
-
 }
